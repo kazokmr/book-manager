f3671444fa3d640f6b7c37757f11df3a76f21e36
Kazuhiko Okumura <engkokmr@gmail.com>
2021-06-06 21:03:41 +0900

    First Commit.

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.5.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.arenagod.gradle.MybatisGenerator") version "1.4"
+    kotlin("jvm") version "1.5.10"
+    kotlin("plugin.spring") version "1.5.10"
+}
+
+group = "com.book.manager"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
+    implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+    runtimeOnly("org.postgresql:postgresql")
+    mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+mybatisGenerator {
+    verbose = true
+    configFile = "$projectDir/src/main/resources/generatorConfig.xml"
+}
