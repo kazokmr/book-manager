6543957e3e124e2fa88c288109c1eb2c103b691c
Kazuhiko Okumura <engkokmr@gmail.com>
2021-06-26 06:56:22 +0900

    TestContainersを利用したRepositoryクラスとMyBatis Mapperの単体テストの実行検証

--- src/main/kotlin/com/book/manager/infrastructure/database/mapper/AccountMapper.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/mapper/AccountMapper.kt
@@ -5,8 +5,10 @@ package com.book.manager.infrastructure.database.mapper
 
 import com.book.manager.infrastructure.database.record.AccountRecord
 import org.apache.ibatis.annotations.DeleteProvider
+import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.InsertProvider
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Param
 import org.apache.ibatis.annotations.Result
 import org.apache.ibatis.annotations.ResultMap
 import org.apache.ibatis.annotations.Results
@@ -23,32 +25,56 @@ import org.mybatis.dynamic.sql.util.SqlProviderAdapter
 
 @Mapper
 interface AccountMapper {
-    @SelectProvider(type=SqlProviderAdapter::class, method="select")
+    @SelectProvider(type = SqlProviderAdapter::class, method = "select")
     fun count(selectStatement: SelectStatementProvider): Long
 
-    @DeleteProvider(type=SqlProviderAdapter::class, method="delete")
+    @DeleteProvider(type = SqlProviderAdapter::class, method = "delete")
     fun delete(deleteStatement: DeleteStatementProvider): Int
 
-    @InsertProvider(type=SqlProviderAdapter::class, method="insert")
+    @InsertProvider(type = SqlProviderAdapter::class, method = "insert")
     fun insert(insertStatement: InsertStatementProvider<AccountRecord>): Int
 
-    @InsertProvider(type=SqlProviderAdapter::class, method="insertMultiple")
+    @InsertProvider(type = SqlProviderAdapter::class, method = "insertMultiple")
     fun insertMultiple(multipleInsertStatement: MultiRowInsertStatementProvider<AccountRecord>): Int
 
-    @SelectProvider(type=SqlProviderAdapter::class, method="select")
+    @SelectProvider(type = SqlProviderAdapter::class, method = "select")
     @ResultMap("AccountRecordResult")
     fun selectOne(selectStatement: SelectStatementProvider): AccountRecord?
 
-    @SelectProvider(type=SqlProviderAdapter::class, method="select")
-    @Results(id="AccountRecordResult", value = [
-        Result(column="id", property="id", jdbcType=JdbcType.BIGINT, id=true),
-        Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
-        Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
-        Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
-        Result(column="role_type", property="roleType", typeHandler=EnumTypeHandler::class, jdbcType=JdbcType.VARCHAR)
-    ])
+    @SelectProvider(type = SqlProviderAdapter::class, method = "select")
+    @Results(
+        id = "AccountRecordResult", value = [
+            Result(column = "id", property = "id", jdbcType = JdbcType.BIGINT, id = true),
+            Result(column = "email", property = "email", jdbcType = JdbcType.VARCHAR),
+            Result(column = "password", property = "password", jdbcType = JdbcType.VARCHAR),
+            Result(column = "name", property = "name", jdbcType = JdbcType.VARCHAR),
+            Result(
+                column = "role_type",
+                property = "roleType",
+                typeHandler = EnumTypeHandler::class,
+                jdbcType = JdbcType.VARCHAR
+            )
+        ]
+    )
     fun selectMany(selectStatement: SelectStatementProvider): List<AccountRecord>
 
-    @UpdateProvider(type=SqlProviderAdapter::class, method="update")
+    @UpdateProvider(type = SqlProviderAdapter::class, method = "update")
     fun update(updateStatement: UpdateStatementProvider): Int
+
+    @Insert(
+        "INSERT INTO account (" +
+                "id," +
+                "email," +
+                "password," +
+                "name," +
+                "role_type" +
+                ") VALUES (" +
+                "#{account.id}," +
+                "#{account.email}," +
+                "#{account.password}," +
+                "#{account.name}," +
+                "#{account.roleType.name}::role_type" +
+                ")"
+    )
+    fun create(@Param("account") accountRecord: AccountRecord): Int
 }
