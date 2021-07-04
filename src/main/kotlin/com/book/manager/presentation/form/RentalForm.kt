db06736fd023daec60444d1e35ba284a240b60c8
Kazuhiko Okumura <engkokmr@gmail.com>
2021-07-05 06:30:58 +0900

    貸出処理の入力内容を検証するようにした

--- src/main/kotlin/com/book/manager/presentation/form/RentalForm.kt
+++ src/main/kotlin/com/book/manager/presentation/form/RentalForm.kt
@@ -2,8 +2,12 @@ package com.book.manager.presentation.form
 
 import com.book.manager.domain.model.Rental
 import java.time.LocalDateTime
+import javax.validation.constraints.Min
 
-data class RentalStartRequest(val bookId: Long)
+data class RentalStartRequest(
+    @field:Min(value = 1, message = "書籍IDには1以上の数値を入れてください。")
+    val bookId: Long
+)
 
 data class RentalStartResponse(
     val bookId: Long,
