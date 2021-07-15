a3f6d30eae2220b9364dfbaa191540fb410119ee
Kazuhiko Okumura <engkokmr@gmail.com>
2021-07-16 06:09:14 +0900

    AOPのエラーロギングテストを追加

--- src/test/kotlin/com/book/manager/presentation/aop/LoggingAdviceTest.kt
+++ src/test/kotlin/com/book/manager/presentation/aop/LoggingAdviceTest.kt
@@ -18,20 +18,25 @@ import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.core.Appender
 import org.apache.logging.log4j.core.LogEvent
 import org.apache.logging.log4j.core.LoggerContext
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.ArgumentCaptor
 import org.mockito.Captor
 import org.springframework.aop.aspectj.annotation.AspectJProxyFactory
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.mock.web.MockHttpSession
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
+import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
@@ -47,16 +52,16 @@ internal class LoggingAdviceTest {
     @MockBean
     private lateinit var bookService: BookService
 
-    // 検査対象となるクラス
+    // AOP(ログ出力)対象クラス
     private lateinit var proxy: BookController
-
+    // Session情報を取得するためのMockリクエスト
     private lateinit var request: MockHttpServletRequest
 
-    // ログの取得用
+    // 検査用のログ出力先となるAppender
+    private lateinit var mockAppender: Appender
+    // 出力したログを保持する
     @Captor
     private lateinit var logMessageCaptor: ArgumentCaptor<LogEvent>
-
-    private lateinit var mockAppender: Appender
 
     @BeforeEach
     internal fun setUp() {
@@ -79,13 +84,22 @@ internal class LoggingAdviceTest {
         whenever(mockAppender.isStarted).thenReturn(true)
         whenever(mockAppender.isStopped).thenReturn(false)
 
-        val ctx = LogManager.getContext(false) as LoggerContext
+        // AppenderモックをLog出力設定に追加
+        val ctx = LoggerContext.getContext(false)
         ctx.configuration.addAppender(mockAppender)
         ctx.configuration.getLoggerConfig(LogManager.ROOT_LOGGER_NAME).apply {
             level = Level.INFO
             addAppender(mockAppender, Level.INFO, null)
         }
         ctx.updateLoggers()
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        // Appenderモックを除去する
+        val ctx = LoggerContext.getContext(false)
+        ctx.configuration.loggers.values.forEach { it.removeAppender(mockAppender.name) }
+        ctx.configuration.rootLogger.removeAppender(mockAppender.name)
     }
 
     @Test
@@ -151,7 +165,26 @@ internal class LoggingAdviceTest {
     }
 
     @Test
+    @DisplayName("例外が投げられたらログレベルErrorのログを出力する")
     fun afterThrowingLog() {
-        // TODO: 例外時のログを確認する
+
+        // Given
+        val bookId: Long = 824
+        val reason = "存在しない書籍: $bookId"
+        whenever(bookService.getDetail(any() as Long)).thenThrow(IllegalArgumentException(reason))
+
+        // When
+        assertThrows<ResponseStatusException> {
+            proxy.getDetail(bookId)
+        }
+
+        // Then
+        verify(mockAppender, times(6)).append(logMessageCaptor.capture())
+        assertThat(logMessageCaptor.allValues.count { it.level == Level.ERROR }).isEqualTo(1)
+
+        val errorLog = logMessageCaptor.allValues.first { it.level == Level.ERROR }
+        assertThat(errorLog.message.formattedMessage)
+            .startsWith("Exception: ")
+            .endsWith("message=${BAD_REQUEST.value()} ${BAD_REQUEST.name} \"$reason\"")
     }
 }
