22d051e95f50caabf65ec9d62ea65deec0ee9ade
Kazuhiko Okumura <engkokmr@gmail.com>
2021-08-15 12:38:00 +0900

    ResponseのJsonをStringからEntityに変換するメソッドをTemplateに追加します。 Beanで変換処理を定義すると、RequestBodyのSnakeCaseに対応できない（対応させると今度はResponseEntityにマッピングできない）ため、Beanを削除し、ResponseEntityはStringで受けた後に変換させることにした。 RequestBodyの方はBeanが無くても正常にJsonからObjectへの変換ができたのでBeanは削除する。

--- src/integration/kotlin/com/book/manager/presentation/config/IntegrationTestConfiguration.kt
+++ src/integration/kotlin/com/book/manager/presentation/config/IntegrationTestConfiguration.kt
@@ -1,11 +1,8 @@
 package com.book.manager.presentation.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 
 @TestConfiguration
 class IntegrationTestConfiguration {
@@ -14,15 +11,5 @@ class IntegrationTestConfiguration {
     fun integrationTestRestTemplate(): IntegrationTestRestTemplate {
         val builder = RestTemplateBuilder(CustomRestTemplateCustomizer())
         return IntegrationTestRestTemplate(builder)
-    }
-
-    // TestRestTemplateを継承したクラスを使うとjackson-module-kotlinのConverterが効かなくなるためBeanを用意する
-    @Bean
-    fun mappingJackson2HttpMessageConverter(): MappingJackson2HttpMessageConverter {
-        return MappingJackson2HttpMessageConverter().apply {
-            this.objectMapper = ObjectMapper().apply {
-                registerKotlinModule()
-            }
-        }
     }
 }
--- src/integration/kotlin/com/book/manager/presentation/config/IntegrationTestRestTemplate.kt
+++ src/integration/kotlin/com/book/manager/presentation/config/IntegrationTestRestTemplate.kt
@@ -1,5 +1,9 @@
 package com.book.manager.presentation.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.MediaType
@@ -24,5 +28,13 @@ class IntegrationTestRestTemplate(builder: RestTemplateBuilder) : TestRestTempla
             .body(loginForm)
 
         return restTemplate.exchange(request, String::class.java)
+    }
+
+    fun <T> jsonStringToObject(json: String, obj: Class<T>): T {
+        return ObjectMapper()
+            .registerKotlinModule()
+            .registerModule(JavaTimeModule())
+            .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
+            .readValue(json, obj)
     }
 }
