9d75dc741dd5f40e32a2d8e8c89b061cc063b9e9
Kazuhiko Okumura <engkokmr@gmail.com>
2022-04-28 21:14:29 +0900

    MyBatis の Dynamic SQLを最新バージョンのAPIに対応しコンパイルエラーを無くします

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/AccountRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/AccountRepositoryImpl.kt
@@ -7,7 +7,6 @@ import com.book.manager.infrastructure.database.mapper.AccountMapper
 import com.book.manager.infrastructure.database.mapper.selectByPrimaryKey
 import com.book.manager.infrastructure.database.mapper.selectOne
 import com.book.manager.infrastructure.database.record.AccountRecord
-import org.mybatis.dynamic.sql.SqlBuilder
 import org.springframework.stereotype.Repository
 
 @Repository
