c91d9617d1780ed602634ec3c251aeb206d3ffb7
kazokmr <engkokmr@gmail.com>
2022-08-24 04:57:11 +0900

    Updated Dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
-    id("org.springframework.boot") version "2.7.2"
+    id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.arenagod.gradle.MybatisGenerator") version "1.4"
     id("jacoco")
@@ -26,12 +26,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-log4j2:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-security:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-aop:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.2")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.3")
+    implementation("org.springframework.boot:spring-boot-starter-log4j2:2.7.3")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.3")
+    implementation("org.springframework.boot:spring-boot-starter-aop:2.7.3")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.3")
     implementation("org.springframework.session:spring-session-data-redis:2.7.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
@@ -41,14 +41,14 @@ dependencies {
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
     implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.0")
     implementation("io.grpc:grpc-kotlin-stub:1.3.0")
-    implementation("io.grpc:grpc-protobuf:1.48.0")
-    implementation("com.google.protobuf:protobuf-kotlin:3.21.3")
-    implementation("io.grpc:grpc-netty:1.48.0")
+    implementation("io.grpc:grpc-protobuf:1.48.1")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.5")
+    implementation("io.grpc:grpc-netty:1.48.1")
     implementation("io.github.lognet:grpc-spring-boot-starter:4.8.0")
-    runtimeOnly("org.postgresql:postgresql:42.3.6")
+    runtimeOnly("org.postgresql:postgresql:42.4.2")
     mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.1")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.2")
-    testImplementation("org.springframework.security:spring-security-test:5.7.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.3")
+    testImplementation("org.springframework.security:spring-security-test:5.7.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
