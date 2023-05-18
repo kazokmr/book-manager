0a21b49e52d8592cde2a0cb239e0a62ae86408fd
kazokmr <engkokmr@gmail.com>
2023-05-19 05:42:26 +0900

    Update version kotlin and Springboot

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.spring") version "1.8.20"
-    id("org.springframework.boot") version "3.0.6"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+    id("org.springframework.boot") version "3.0.7"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
     id("jacoco")
