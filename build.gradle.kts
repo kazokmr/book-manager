c49219e70eecc3e1f9b2ea5981e804d6974067c7
kazokmr <engkokmr@gmail.com>
2023-06-09 05:13:03 +0900

    Kotlin1.8.22 に更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.thinkimi.gradle.MybatisGenerator") version "2.4"
