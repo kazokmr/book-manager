3195a50bc6e234d3d0cf8406e1f9c509bced41ff
kazokmr <engkokmr@gmail.com>
2023-08-25 05:22:42 +0900

    Update Spring boot to 3.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.10"
     kotlin("plugin.spring") version "1.9.10"
-    id("org.springframework.boot") version "3.1.1"
+    id("org.springframework.boot") version "3.1.2"
     id("io.spring.dependency-management") version "1.1.0"
     id("com.qqviaja.gradle.MybatisGenerator") version "2.5"
     id("jacoco")
